728d932a792fa325bd32813a9f3616f670b21b9b
rmaskey1 <maskey.reshaj@gmail.com>
2025-07-15 14:43:44 -0700

    webapp setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import InterestForm from './components/InterestForm'
+
+function App() {
+
+  return (
+   <InterestForm/>
+  )
+}
+
+export default App
