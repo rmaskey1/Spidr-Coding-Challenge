b2e902a7f126010a88a2f62c8f44bb1152c19dec
rmaskey1 <maskey.reshaj@gmail.com>
2025-07-15 17:59:59 -0700

    background air fryer addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
 import './App.css'
 import InterestForm from './components/InterestForm'
+import airFryerImage from './assets/air-fryer.jpg'
 
 function App() {
 
   return (
-   <InterestForm/>
+    <div className="form-image-wrapper">
+      <div className="image-container">
+        <img src={airFryerImage} alt="Air Fryer" />
+      </div>
+      <div className="form-container">
+        <InterestForm />
+      </div>
+    </div>
   )
 }
 
